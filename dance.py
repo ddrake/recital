ba3b4a1f95e0dbf0803d4bd3fd64206ab66ca757
Dow Drake <dowdrake@msn.com>
2018-04-27 15:59:52 -0700

    work in progress

--- dance.py
+++ dance.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 import random
 import sys
+import argparse
 
 class Dance:
     def __init__(self, dancers, title="Untitled"):
@@ -11,11 +12,12 @@ class Dance:
         return "{}: {}".format(self.title, str(self.dancers))
 
 class DanceSequence:
-    def __init__(self, dances=None):
+    def __init__(self, dances=None, order=None):
         if not dances:
             raise ValueError("Each sequence must contain at least one dance")
         
         self.dances = dances
+        self.order = order
 
     def __str__(self):
         return ", ".join([str(dance) for dance in self.dances])
@@ -86,34 +88,47 @@ def make_test_data():
 
     return seqs
 
-# Program starts here
-if len(sys.argv) > 1:
-    try:
-        with open(sys.argv[1],'r') as f:
-            contents = f.read()
-    except Exception as ex:
-        print(ex)
-        sys.exit(2)
+def parse_line(line):
+    seqinfo = line.strip().split('|')
+    if len(seqinfo) == 2:
+        order, dances = seqinfo
+    elif len(seqinfo) == 1:
+        order, dances = None, seqinfo[0]
+    else: raise Exception("Can't parse line")
 
+# Program starts here
+parser = argparse.ArgumentParser(description='Compute some possible programs for a recital.')
+parser.add_argument('-f', help='sequence file path')
+parser.add_argument('-n', type=int, default=0, help='number of allowed overlaps')
+parser.add_argument('-a', action='store_true', \
+        help="show all programs, even those which don't satisfy ordering")
+ns = parser.parse_args(sys.argv)
+include_all = ns.a
+infile      = ns.f
+overlaps    = ns.n
+
+if infile:
     try:
         lines = contents.strip().split('\n')
         lines = [line for line in lines if line and line[0] != '#']
         seqs = []
         for line in lines:
-            dances = line.strip().split(';')
+            parse_line(line)
+            order, dances = parse_line(line)
+            dances = dances.strip().split(';')
             seq = []
             for d in dances:
                 dinfo = d.strip().split(':')
                 if len(dinfo) == 2:
                     title, dancers = dinfo
                 elif len(dinfo) == 1:
-                    title, dancers = None, dinfo
-                else: raise Exception("Can't parse input file")
+                    title, dancers = None, dinfo[0]
+                else: raise Exception("Can't parse dance")
 
                 dance = Dance(set(dancers.strip().split()), title)
                 seq.append(dance)
 
-            seqs.append(DanceSequence(seq))
+            seqs.append(DanceSequence(seq, order=order))
 
     except Exception as ex:
         print(ex)
