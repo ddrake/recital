f40ea222051aaf652c6ebfe9f75b829e2693c295
Dow Drake <dowdrake@msn.com>
2018-04-27 09:57:38 -0700

    cleanup

--- dance.py
+++ dance.py
@@ -2,6 +2,14 @@
 import random
 import sys
 
+class Dance:
+    def __init__(self, dancers, title="Untitled"):
+        self.dancers = dancers # a set of dancer names
+        self.title = title
+
+    def __str__(self):
+        return "{}: {}".format(self.title, str(self.dancers))
+
 class DanceSequence:
     def __init__(self, dances=None):
         if not dances:
@@ -10,10 +18,10 @@ class DanceSequence:
         self.dances = dances
 
     def __str__(self):
-        return ", ".join([str(d) for d in self.dances])
+        return ", ".join([str(dance) for dance in self.dances])
 
     def isdisjoint(self, sequence):
-        return self.dances[-1].isdisjoint(sequence.dances[0])
+        return self.dances[-1].dancers.isdisjoint(sequence.dances[0].dancers)
 
 # a program is a valid list of DanceSequences
 class Program:
@@ -50,25 +58,25 @@ def make_test_data():
     dancers = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p']
     dances = []
     random.shuffle(dancers)
-    dances.append(set(dancers[:3]))
+    dances.append(Dance(set(dancers[:3]),"Dance 1"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:5]))
+    dances.append(Dance(set(dancers[:5]),"Dance 2"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:2]))
+    dances.append(Dance(set(dancers[:2]),"Dance 3"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:4]))
+    dances.append(Dance(set(dancers[:4]),"Dance 4"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:7]))
+    dances.append(Dance(set(dancers[:7]),"Dance 5"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:3]))
+    dances.append(Dance(set(dancers[:3]),"Dance 6"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:1]))
+    dances.append(Dance(set(dancers[:1]),"Dance 7"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:5]))
+    dances.append(Dance(set(dancers[:5]),"Dance 8"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:6]))
+    dances.append(Dance(set(dancers[:6]),"Dance 9"))
     random.shuffle(dancers)
-    dances.append(set(dancers[:5]))
+    dances.append(Dance(set(dancers[:5]),"Dance 10"))
 
     seqs = []
     seqs.append(DanceSequence([dances[0],dances[1],dances[2]]))
@@ -76,10 +84,9 @@ def make_test_data():
     for i in range(5,len(dances)):
         seqs.append(DanceSequence([dances[i]]))
 
-    for s in seqs:
-        print(s)
     return seqs
 
+# Program starts here
 if len(sys.argv) > 1:
     try:
         with open(sys.argv[1],'r') as f:
@@ -93,9 +100,21 @@ if len(sys.argv) > 1:
         lines = [line for line in lines if line and line[0] != '#']
         seqs = []
         for line in lines:
-            tseqs = line.strip().split(';')
-            seqs.append(DanceSequence([set(tseq.strip().split()) for tseq in tseqs if tseq]))
-            #print([set(tseq.strip().split()) for tseq in tseqs])
+            dances = line.strip().split(';')
+            seq = []
+            for d in dances:
+                dinfo = d.strip().split(':')
+                if len(dinfo) == 2:
+                    title, dancers = dinfo
+                elif len(dinfo) == 1:
+                    title, dancers = None, dinfo
+                else: raise Exception("Can't parse input file")
+
+                dance = Dance(set(dancers.strip().split()), title)
+                seq.append(dance)
+
+            seqs.append(DanceSequence(seq))
+
     except Exception as ex:
         print(ex)
         sys.exit(2)
@@ -106,7 +125,7 @@ for s in seqs:
     print(s)
 
 programs = step([],seqs)
-print(len(programs))
+print("\n{0:d} program(s) found.".format(len(programs)))
 for p in programs:
     print(p)
 
